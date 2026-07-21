5c970828738954d84150d2a55d363c1644b6a3e5
gabriel angelo <gabriel.angelo7002@gmail.com>
2026-07-21 16:36:00 -0300

    2107pt2

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ function abrirmenu() {
 function fecharmenu() {
     const menu = document.getElementById("menubar");
     menu.classList.remove("open");
+
 }
 
-
-const botao = document.querySelector(".botao");
-botao.addEventListener("click", clicar)
-
-function clicar() {
-    alert("você clicou no botão");
+function xre190() {
+    const banner = document.querySelector(".banner");
+    banner.classList.remove("teste");
+    banner.classList.add("xre190");
 }
