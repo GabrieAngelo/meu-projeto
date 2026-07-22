eb088874f1e537f2d46b2609d888f654e267e7f5
gabriel angelo <gabriel.angelo7002@gmail.com>
2026-07-22 16:07:30 -0300

    xre300

--- script.js
+++ script.js
@@ -9,8 +9,32 @@ function fecharmenu() {
 
 }
 
-function xre190() {
+
+const motos = document.querySelector(".motos");
+
+motos.addEventListener("mouseenter", () => {
     const banner = document.querySelector(".banner");
     banner.classList.remove("teste");
     banner.classList.add("xre190");
-}
+});
+motos.addEventListener("mouseleave", () => {
+    const banner = document.querySelector(".banner");
+    banner.classList.remove("xre190");
+    banner.classList.add("teste");
+
+});
+
+const xre300 = document.querySelector("#xre300")
+
+xre300.addEventListener("mouseenter", () => {
+    const banner = document.querySelector("banner")
+    banner.classList.remove("teste");
+    banner.classList.add("xre300");
+
+});
+xre300.addEventListener("mouseleave", () => {
+    const banner = document.querySelector("banner")
+    banner.classList.remove("xre300");
+    banner.classList.add("teste");
+
+});
